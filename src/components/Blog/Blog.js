a1d80316a416e70d69e0151d1cb7daf2a1484106
unknown <rich.nayan@gmail.com>
2022-10-12 13:54:57 +0600

    all done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
 
 const Blog = () => {
     return (
         <div>
-            <h2>This is Blog page</h2>
+            <h2>Important Question</h2>
+            <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What are the purpose of React Router</Accordion.Header>
+        <Accordion.Body>
+        React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application. It helps  navigation and routing functionalities. It helps  redirect to another page in ReactJS using react-router-dom package.It's saves our time and make us hassle free
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
         </div>
     );
 };
--- src/components/Statastics/Statastics.js
+++ src/components/Statastics/Statastics.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } from 'recharts';
 
 
 const Statastics = () => {
     const data = useLoaderData();
-    const total = data.data
-    console.log(total)
+    const chartValue = data.data
+    console.log(chartValue)
     return (
-        <div>
+        <div className='chart-Container'>
             <h2>This Statastics page{data.data.length}</h2>
-            <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={data}>
-                <Bar dataKey="total" fill="#8884d8" />
-                 </BarChart>
-            </ResponsiveContainer>
+            <LineChart width={400} height={400} data={chartValue}>
+            <Line type="monotone" dataKey="total" stroke="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            </LineChart>
         </div>
     );
 };
